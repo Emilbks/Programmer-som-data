d83882092dac934844aae5cca941efe04337078f
= <embs@itu.dk>
2025-08-31 15:14:33 +0200

    Exercise 1.1

--- Lecture 01/Intro/Intro2.fs
+++ Lecture 01/Intro/Intro2.fs
@@ -6,16 +6,16 @@ module Intro2
 
 (* Association lists map object language variables to their values *)
 
-let env = [("a", 3); ("c", 78); ("baf", 666); ("b", 111)];;
+let env = [("a", 3); ("c", 78); ("baf", 666); ("b", 111)]
 
-let emptyenv = []; (* the empty environment *)
+let emptyenv = [] (* the empty environment *)
 
 let rec lookup env x =
     match env with 
     | []        -> failwith (x + " not found")
-    | (y, v)::r -> if x=y then v else lookup r x;;
+    | (y, v)::r -> if x=y then v else lookup r x
 
-let cvalue = lookup env "c";;
+let cvalue = lookup env "c"
 
 
 (* Object language expressions with variables *)
@@ -23,27 +23,54 @@ let cvalue = lookup env "c";;
 type expr = 
   | CstI of int
   | Var of string
-  | Prim of string * expr * expr;;
+  | Prim of string * expr * expr
+  | If of expr * expr * expr
 
-let e1 = CstI 17;;
+let e1 = CstI 17
 
-let e2 = Prim("+", CstI 3, Var "a");;
+let e2 = Prim("+", CstI 3, Var "a")
 
-let e3 = Prim("+", Prim("*", Var "b", CstI 9), Var "a");;
+let e3 = Prim("+", Prim("*", Var "b", CstI 9), Var "a")
+
+let eMax = Prim("max", CstI 5, CstI 7)
+
+let eMin = Prim("min", Prim ("-", CstI 8, CstI 3), CstI 7)
+
+let eEqTrue = Prim("==", Prim ("+", CstI 6, CstI 4), Prim ("*", CstI 2, CstI 5))
+
+let eEqFalse = Prim("==", Prim ("-", CstI 6, CstI 4), Prim ("*", CstI 2, CstI 5))
+
+let eIf = If (Var "a", CstI 11, CstI 22)
 
 
 (* Evaluation within an environment *)
 
 let rec eval e (env : (string * int) list) : int =
     match e with
-    | CstI i            -> i
-    | Var x             -> lookup env x 
-    | Prim("+", e1, e2) -> eval e1 env + eval e2 env
-    | Prim("*", e1, e2) -> eval e1 env * eval e2 env
-    | Prim("-", e1, e2) -> eval e1 env - eval e2 env
-    | Prim _            -> failwith "unknown primitive";;
+    | CstI i -> i
+    | Var x -> lookup env x 
+    | Prim (ope, e1, e2) ->
+        let i1 = eval e1 env
+        let i2 = eval e2 env
+        match ope with
+        | "+" -> i1 + i2
+        | "-" -> i1 - i2
+        | "*" -> i1 * i2
+        | "max" -> max i1 i2
+        | "min" -> min i1 i2
+        | "==" -> if i1 = i2 then 1 else 0
+        | _ -> failwith "unknown primitive"
+    | If (e1, e2, e3) ->
+        if eval e1 env <> 0 then eval e2 env else eval e3 env
 
-let e1v  = eval e1 env;;
-let e2v1 = eval e2 env;;
-let e2v2 = eval e2 [("a", 314)];;
-let e3v  = eval e3 env;;
+
+let e1v  = eval e1 env
+let e2v1 = eval e2 env
+let e2v2 = eval e2 [("a", 314)]
+let e3v  = eval e3 env
+let eMaxv = eval eMax env
+let eMinv = eval eMin env
+let eEqTruev = eval eEqTrue env
+let eEqFalsev = eval eEqFalse env
+let eIf11 = eval eIf env
+let eIf22 = eval eIf [("a", 0)]
