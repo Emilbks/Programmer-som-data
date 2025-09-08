a802f143332080005d5c4e522a7d652711c1710d
Emil <embs@itu.dk>
2025-09-08 14:55:51 +0200

    2.4, 3.2

--- Lecture 02/ex2_4Handout.fs
+++ Lecture 02/ex2_4Handout.fs
@@ -1,17 +1,24 @@
+// 2.4
+module Ex2_4Handout
 
+open Expr
 
 (* Ex 2.4 - assemble to integers *)
 (* SCST = 0, SVAR = 1, SADD = 2, SSUB = 3, SMUL = 4, SPOP = 5, SSWAP = 6; *)
-let sinstrToInt = function
-  | SCstI i -> [0;i]
-  | SVar i  -> failwith "Not implemented"
-  | SAdd    -> failwith "Not implemented"
-  | SSub    -> failwith "Not implemented"
-  | SMul    -> failwith "Not implemented"
-  | SPop    -> failwith "Not implemented"
-  | SSwap   -> failwith "Not implemented"
+let rec sinstrToInt (exp : sinstr) = 
+    match exp with
+    | SCstI i -> [0;i]
+    | SVar i  -> [1;i]
+    | SAdd    -> [2]
+    | SSub    -> [3]
+    | SMul    -> [4]
+    | SPop    -> [5]
+    | SSwap   -> [6]
 
-let assemble instrs = failwith "Not implemented"
+let assemble (instrs : sinstr list) : int list =
+    let rec loop instrs out = match instrs with | [] -> out | s::ss -> loop ss out@(sinstrToInt s)
+    loop (instrs |> List.rev) []
+
 
 (* Output the integers in list inss to the text file called fname: *)
 
---
+++ Lecture 02/load-project.fsx
@@ -0,0 +1,6 @@
+﻿System.IO.Directory.SetCurrentDirectory(@"C:\Users\ebksd\Documents\Programmer-som-data\Lecture 02");;
+
+#load "Expr/Absyn.fs"
+#load "Expr/Expr.fs"
+#load "./ex2_4Handout.fs"
+open Ex2_4Handout;;
