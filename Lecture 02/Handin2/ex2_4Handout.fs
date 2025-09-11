b8b7898f32c6656c2eaaec475a1e54f0d639ba92
Emil <embs@itu.dk>
2025-09-11 09:48:23 +0200

    fixed 3.2

---
+++ Lecture 02/Handin2/ex2_4Handout.fs
@@ -0,0 +1,27 @@
+// 2.4
+module Ex2_4Handout
+
+open Expr
+
+(* Ex 2.4 - assemble to integers *)
+(* SCST = 0, SVAR = 1, SADD = 2, SSUB = 3, SMUL = 4, SPOP = 5, SSWAP = 6; *)
+let rec sinstrToInt (exp : sinstr) = 
+    match exp with
+    | SCstI i -> [0;i]
+    | SVar i  -> [1;i]
+    | SAdd    -> [2]
+    | SSub    -> [3]
+    | SMul    -> [4]
+    | SPop    -> [5]
+    | SSwap   -> [6]
+
+let assemble (instrs : sinstr list) : int list =
+    let rec loop instrs out = match instrs with | [] -> out | s::ss -> loop ss out@(sinstrToInt s)
+    loop (instrs |> List.rev) []
+
+
+(* Output the integers in list inss to the text file called fname: *)
+
+let intsToFile (inss : int list) (fname : string) = 
+    let text = String.concat " " (List.map string inss)
+    System.IO.File.WriteAllText(fname, text);;
