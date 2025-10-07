5e99d76a199e86ea58d131e92873bd1635c4fcba
Adam <adni@itu.dk>
2025-10-07 13:57:40 +0200

    exericse 7.1 & 7.2 beginning

---
+++ Lecture_06/MicroC/ex71iii.c
@@ -0,0 +1,18 @@
+void main () {
+    int arr[7];
+    arr[0] = 1;
+    arr[1] = 2;
+    arr[2] = 1;
+    arr[3] = 1;
+    arr[4] = 1;
+    arr[5] = 2;
+    arr[6] = 0;
+
+    int freq[4];
+
+    histogram(7, arr, 3, freq);
+}
+
+void histogram(int n, int ns[], int max, int freq[]) {
+
+}
