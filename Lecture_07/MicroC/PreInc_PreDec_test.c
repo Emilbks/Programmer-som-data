c97da7aab319d4818cdf4f53b7d75239964013ea
Akn <adni@itu.dk>
2025-10-27 11:21:05 +0100

    exercise 8.1 and 8.3

---
+++ Lecture_07/MicroC/PreInc_PreDec_test.c
@@ -0,0 +1,10 @@
+void main(int n) {
+	int i;
+	i = 0;
+
+	++i;
+	print i;
+
+	--i;
+	print i;
+}
---
+++ Lecture_07/MicroC/fsharpi.bat
@@ -0,0 +1,2 @@
+@echo off
+dotnet fsi -r ".\..\..\fsharp\FsLexYacc.Runtime.11.3.0\lib\netstandard2.0\FsLexYacc.Runtime.dll" %*
